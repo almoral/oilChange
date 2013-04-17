8efc43ae977139ae6571c9b8fadb7b6b52fe30aa
almoral <alexmorales30@gmail.com>
2013-04-17 08:16:23 -0400

    Updating db

--- db/migrate/20130411151554_create_cars.rb
+++ db/migrate/20130411151554_create_cars.rb
@@ -1,7 +1,10 @@
 class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
-
+      t.integer :year
+      t.string :make
+      t.string :model
+      t.integer :mileage
       t.timestamps
     end
   end
